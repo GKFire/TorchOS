0e6621bb0270b9a81ecb84467137fdee54b25bb1
Raven Wyssen <ravenwyssen@gmail.com>
2022-06-23 08:18:10 -0600

    Update debian_install.sh

--- debian_install.sh
+++ debian_install.sh
@@ -31,61 +31,6 @@ cd ../dmenu-5.0/
 make clean install
 cd ../
 
-
-# install Brave Browser
-curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"| tee /etc/apt/sources.list.d/brave-browser-release.list
-apt update
-apt -y install brave-browser
-
-
-# install Visual Studio Code
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
-sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-rm -f packages.microsoft.gpg
-apt update
-apt -y install code
-
-
-
-
-# install Rust
-echo ""
-echo ""
-echo "Would you like to have Rust installed on this machine?"
-echo "'1': Yes"
-echo "'0': No"
-echo "(default value is 0, invaild responce will equal 0.)"
-read installRust
-
-if [ "$installRust" == "1" ]; then
-	curl https://sh.rustup.rs -sSf | sh
-fi
-
-
-# install Java and Eclipse as its IDE
-echo ""
-echo ""
-echo "Would you like to have Java + Eclipse installed on this machine?"
-echo "'1': Yes"
-echo "'0': No"
-echo "(default value is 0, invaild responce will equal 0.)"
-echo "NOTE: this will install both the latest version and lts version of openjdk. (Latest: openjdk-17 | LTS: openjdk-17)"
-read installJava
-
-if [ "$installJava" == "1" ]; then
-	apt -y install openjdk-17-jdk
-	cd /usr/share/
-	# debian doesn't have eclipse in it's repositories. So we need to download it and set it up manually.
-	wget https://mirrors.jevincanders.net/eclipse/technology/epp/downloads/release/2021-12/R/eclipse-java-2021-12-R-linux-gtk-x86_64.tar.gz
-	tar -xvf eclipse-java-2021-12-R-linux-gtk-x86_64.tar.gz
-	rm eclipse-java-2021-12-R-linux-gtk-x86_64.tar.gz
-	mv eclipse-java-2021-12-R-linux-gtk-x86_64 eclipse
-	cd /opt/TorchOS/
-fi
-
-
 # copy .xinitrc file and correct permission
 echo "setting up .xinitrc"
 cp .xinitrc /home/$USERNAMEVAR/.xinitrc
@@ -103,11 +48,22 @@ chown $USERNAMEVAR.$USERNAMEVAR /home/$USERNAMEVAR/.config/fish
 chown $USERNAMEVAR.$USERNAMEVAR /home/$USERNAMEVAR/.config/fish/config.fish
 chown $USERNAMEVAR.$USERNAMEVAR /home/$USERNAMEVAR/.config/fish/fish_variables
 
-# auto create alias for eclipse because laziness
-if [ "$installJava" == "1" ]; then
-	echo "alias eclipse='/usr/share/eclipse/eclipse'" >> /home/$USERNAMEVAR/.config/fish/config.fish
 
-fi
+# install Brave Browser
+curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"| tee /etc/apt/sources.list.d/brave-browser-release.list
+apt update
+apt -y install brave-browser
+
+
+# install Visual Studio Code
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+apt update
+apt -y install code
+
 
 echo ""
 echo ""
